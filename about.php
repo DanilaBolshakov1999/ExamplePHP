9cf4cf24730796f9d02cbb4c94efda9ee559b8ec
Danila Bolshakov <danila1999777@gmail.com>
2023-11-29 11:09:48 +0300

    Commit Update

--- about.php
+++ about.php
@@ -2,7 +2,16 @@
     $title = "Про нас";
     require_once "blocks/header.php";
 ?>
-<h1>Про нас</h1>
+<div class="container mt-2">
+    <h1>Про нас</h1>
+    <form action="blocks/check_get.php" method="get">
+        <input type="text" name="username" placeholder="Введите имя" class="form-control"><br>
+        <input type="email" name="email" placeholder="Введите email" class="form-control"><br>
+        <input type="password" name="password" placeholder="Введите пароль" class="form-control"><br>
+        <textarea name="message" placeholder="Введите сообщение" class="form-control"></textarea><br>
+        <input type="submit" value="Отправить" class="btn btn-success">
+    </form>
+</div>
 <?php
     require "blocks/footer.php";
 ?>
--- connecting.php
+++ connecting.php
@@ -2,7 +2,8 @@
     $title = "Главная страница";
     require "blocks/header.php";
 ?>
-<h1>Главная страница</h1>
+    <h1>Главная страница</h1>
 <?php
+    print_r($_GET);
     require "blocks/footer.php";
 ?>
